de3ba0ff4ee7d839ff076f85c35777f0359c36bc
Chuanxun <chuanxun@chuanxuntekimac-mini.local>
2016-03-28 10:07:42 +0800

    Initial Commit

---
+++ TrackMySelfTests/TrackMySelfTests.swift
@@ -0,0 +1,36 @@
+//
+//  TrackMySelfTests.swift
+//  TrackMySelfTests
+//
+//  Created by Chuanxun on 16/3/28.
+//  Copyright © 2016年 Leon. All rights reserved.
+//
+
+import XCTest
+@testable import TrackMySelf
+
+class TrackMySelfTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
